62e944e311fac546d8d0803035433484eea2f252
martinezf <martinezf1985gmail.com>
2021-09-05 21:09:58 -0300

    router

--- meiarmonia/src/componentes/ItemCount.js
+++ meiarmonia/src/componentes/ItemCount.js
@@ -1,28 +1,47 @@
 import React, { useState } from "react";
-  
-const ItemCount = ({stock, initial, onAdd}) => {
-    const [count, setCount] = useState(initial);
-    return (
-        <div >
-            <div>
-              <div>{count} {onAdd} </div>
-            {/* <h1>contador: </h1> */}
-                
-              <button className='but'  onClick={() => count < stock ? setCount(count + 1) : ''}> + </button>
-              <button className='but'  onClick={() => count >= 1? setCount(  count - 1) : ''  } > - </button> 
-            </div>
 
-            <div>
-              <button className='but'  onClick={onAdd}> Add </button> 
-                
-            </div>
-          
+
+const ItemCount = ({ stock, initial, onAdd }) => {
+  const [count, setCount] = useState(initial);
+  const NumberContext = React.createContext();
+  return (
+    
+    <div>
+      <NumberContext.Consumer>
+      {value => <div>count {count}.</div>}
+    </NumberContext.Consumer>
+      <div>
+        <div>
+          {count} {onAdd}{" "}
         </div>
-      );
-    }
-ItemCount.propTypes = {
-  
+        {/* <h1>contador: </h1> */}
 
-}
+        <button
+          className="but"
+          onClick={() => (count < stock ? setCount(count + 1) : "")}
+        >
+          {" "}
+          +{" "}
+        </button>
+        <button
+          className="but"
+          onClick={() => (count >= 1 ? setCount(count - 1) : "")}
+        >
+          {" "}
+          -{" "}
+        </button>
+      </div>
 
-export default ItemCount
+      <div>
+        <button className="but" onClick={onAdd}>
+          {" "}
+          Add{" "}
+        </button>
+      </div>
+      
+    </div>
+  );
+};
+ItemCount.propTypes = {};
+
+export default ItemCount;
--- meiarmonia/src/componentes/ItemDetail.js
+++ meiarmonia/src/componentes/ItemDetail.js
@@ -1,5 +1,13 @@
+import React, {useState} from 'react'
+import ItemCount from './ItemCount';
+
 function ItemDetail({ id, title, description, stock, pictureUrl, price }) {
+  const [onAdd, setOnAdd]= useState (0);
+  const NumberContext = React.createContext();
   return (
+    <NumberContext.Provider value={42}>
+        
+      
     <div className={"item-list"}>
       <div className={"card"}>
         <ul>
@@ -11,9 +19,11 @@ function ItemDetail({ id, title, description, stock, pictureUrl, price }) {
           <li>{description}</li>
           <li>$ {price}</li>
           <li>Available stock: {stock}</li>
+          <ItemCount stock={0} initial={1} onAdd={console.log()} />
         </ul>
       </div>
     </div>
+    </NumberContext.Provider>
   );
 }
 
