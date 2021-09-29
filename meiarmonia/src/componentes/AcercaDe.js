028639d39f9fa8b8917ffa7b84b6aa098dd784c5
martinezf <martinezf1985gmail.com>
2021-09-29 19:27:09 -0300

    trabaj final

--- meiarmonia/src/componentes/AcercaDe.js
+++ meiarmonia/src/componentes/AcercaDe.js
@@ -4,9 +4,9 @@ export default function AcercaDe() {
     return (
         <div className="jumbotron">
             <h1 className="display-4">Acerca de Nosotros</h1>
-            <p className="lead">This is a simple hero unit, a simple jumbotron-style component for calling extra attention to featured content or information.</p>
+            <p className="lead">hernannnnn  aprobameeeee que me costo uno y la mitad del otro!!!</p>
             <hr className="my-4" />
-            <p>It uses utility classNamees for typography and spacing to space content out within the larger container.</p>
+            <p>soy un estudiante de programador que quiere ganar una gerra con un chuchillo y una granada</p>
             <Link className="btn btn-primary btn-lg" to="/" role="button">Inicio</Link>
         </div>
     )
